15b40739bac819ae54ea23d3255d162a3d1239ea
Akshat Khatri <jitikhatri@gmail.com>
2024-05-12 20:44:43 +0530

    matrix_operations

--- matrix.c
+++ matrix.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Function prototypes
 void transposeMatrix(int mainMatrix[3][3], int row, int col);
 void additionOfTwoMatrix(int mainMatrix1[3][3], int mainMatrix2[3][3], int row, int col);
 void productOfTwoMatrix(int mainMatrix1[3][3], int mainMatrix2[3][3], int row, int col);
-void printMatrix(int mainMatrix[3][3],int row, int col);
+void printMatrix(int mainMatrix[3][3], int row, int col);
 
 int main()
 {
@@ -13,91 +14,121 @@ int main()
     int mainMatrix[3][3];
     int mainMatrix2[3][3];
 
+    // Input values for matrix1 and matrix2
+    printf("Enter values for matrix1:\n");
     for (int i = 0; i < row; i++)
     {
         for (int j = 0; j < col; j++)
         {
-            printf("\nEnter value for MATRIX 1  [%d][%d]: ", i, j);
+            printf("Enter value for matrix1[%d][%d]: ", i, j);
             scanf("%d", &mainMatrix[i][j]);
-
-            printf("\nenter value for MATRIX 2  [%d][%d] ",i,j);
-            scanf("%d",&mainMatrix2[i][j]);
         }
     }
 
+    printf("\nEnter values for matrix2:\n");
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            printf("Enter value for matrix2[%d][%d]: ", i, j);
+            scanf("%d", &mainMatrix2[i][j]);
+        }
+    }
 
+    // Select operation
     int input;
+    printf("\nPress 1 for transpose\nPress 2 for addition\nPress 3 for product\n");
+    scanf("%d", &input);
 
-    printf("press 1 for transpose \n press 2 for addition \n press 3 for multiplication\n");
-    scanf("%d",&input);
-
-    if(input ==1)
+    // Perform selected operation
+    if (input == 1)
         transposeMatrix(mainMatrix, row, col);
-
-    else if(input==2)
+    else if (input == 2)
         additionOfTwoMatrix(mainMatrix, mainMatrix2, row, col);
-
-    else if(input==3)
+    else if (input == 3)
         productOfTwoMatrix(mainMatrix, mainMatrix2, row, col);
-
     else
-    printf("wrong input");
+        printf("Wrong input");
 
     return 0;
 }
-void printMatrix(int mainMatrix[3][3], int row, int col){
 
-    for(int i=0;i<row;i++)
+// Function to print a matrix
+void printMatrix(int mainMatrix[3][3], int row, int col)
+{
+    printf("Resulting matrix:\n");
+    for (int i = 0; i < row; i++)
     {
-        for(int j=0;j<col;j++)
+        for (int j = 0; j < col; j++)
         {
-            printf("%d ",mainMatrix[i][j]);
+            printf("%d ", mainMatrix[i][j]);
         }
         printf("\n");
     }
 }
+
+// Function to calculate the transpose of a matrix
 void transposeMatrix(int mainMatrix[3][3], int row, int col)
 {
     int resultMatrix[3][3];
 
+    // Calculate transpose
     for (int i = 0; i < row; i++)
     {
         for (int j = 0; j < col; j++)
         {
-            resultMatrix[i][j]= mainMatrix[j][i];
+            resultMatrix[i][j] = mainMatrix[j][i];
         }
     }
 
-    printMatrix(resultMatrix,row,col);
+    // Print result
+    printMatrix(resultMatrix, row, col);
 }
 
-void additionOfTwoMatrix(int mainMatrix[3][3], int mainMatrix2[3][3], int row, int col)
+// Function to calculate the addition of two matrices
+void additionOfTwoMatrix(int mainMatrix1[3][3], int mainMatrix2[3][3], int row, int col)
 {
     int resultMatrix[3][3];
 
-    for(int i=0;i<row;i++)
+    // Calculate addition
+    for (int i = 0; i < row; i++)
     {
-        for(int j=0;j<col;j++)
+        for (int j = 0; j < col; j++)
         {
-            resultMatrix[i][j]=mainMatrix[i][j] + mainMatrix2[i][j];
+            resultMatrix[i][j] = mainMatrix1[i][j] + mainMatrix2[i][j];
         }
-        printf("\n");
     }
 
-    printMatrix(resultMatrix,row,col);
+    // Print result
+    printMatrix(resultMatrix, row, col);
 }
 
-void productOfTwoMatrix(int mainMatrix1[3][3], int mainMatrix2[3][3], int row, int col) {
+// Function to calculate the product of two matrices
+void productOfTwoMatrix(int mainMatrix1[3][3], int mainMatrix2[3][3], int row, int col)
+{
     int resultMatrix[3][3];
 
-    for (int i = 0; i < row; i++) {
-        for (int j = 0; j < col; j++) {
+    // Initialize result matrix to zeros
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
             resultMatrix[i][j] = 0;
-            for (int k = 0; k < col; k++) {
+        }
+    }
+
+    // Calculate product
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            for (int k = 0; k < col; k++)
+            {
                 resultMatrix[i][j] += mainMatrix1[i][k] * mainMatrix2[k][j];
             }
         }
     }
 
+    // Print result
     printMatrix(resultMatrix, row, col);
 }
